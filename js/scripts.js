cca5c61678c701d3db369de2dc90969e2be62195
Mac-ziis <macg286@gmail.com>
2023-06-10 09:58:13 -0700

    Add the js for the temperature converter to html index file with a script tag in the body of the code this is the only way that it will work

--- js/scripts.js
+++ js/scripts.js
@@ -5,30 +5,30 @@ function kiloweightConvert(value) {
   document.getElementById("stones").innerHTML = value * 0.1574;
 }
 
-let celsius = document.getElementById("celsius");
-let fahrenheit = document.getElementById("fahrenheit");
-let kelvin = document.getElementById("kelvin");
+// let celsius = document.getElementById("celsius");
+// let fahrenheit = document.getElementById("fahrenheit");
+// let kelvin = document.getElementById("kelvin");
 
-celsius.oninput = function () {
-  let f = (parseFloat(celsius.value) * 9) / 5 + 32;
-  fahrenheit.value = parseFloat(f.toFixed(2));
+// celsius.oninput = function () {
+//   let f = (parseFloat(celsius.value) * 9) / 5 + 32;
+//   fahrenheit.value = parseFloat(f.toFixed(2));
 
-  let k = (parseFloat(celsius.value) + 273.15);
-  kelvin.value = parseFloat(k.toFixed(2));
-}
+//   let k = (parseFloat(celsius.value) + 273.15);
+//   kelvin.value = parseFloat(k.toFixed(2));
+// }
 
-fahrenheit.oninput = function () {
-  let c = ((parseFloat(fahrenheit.value) - 32) * 5) / 9;
-  celsius.value = parseFloat(c.toFixed(2));
+// fahrenheit.oninput = function () {
+//   let c = ((parseFloat(fahrenheit.value) - 32) * 5) / 9;
+//   celsius.value = parseFloat(c.toFixed(2));
 
-  let k = (parseFloat(fahrenheit.value) - 32) * 5 / 9 + 273.15;
-  kelvin.value = parseFloat(k.toFixed(2));
-}
+//   let k = (parseFloat(fahrenheit.value) - 32) * 5 / 9 + 273.15;
+//   kelvin.value = parseFloat(k.toFixed(2));
+// }
 
-kelvin.oninput = function () {
-  let f = (parseFloat(kelvin.value) - 273.15) * 9 / 5 + 32;
-  fahrenheit.value = parseFloat(f.toFixed(2));
+// kelvin.oninput = function () {
+//   let f = (parseFloat(kelvin.value) - 273.15) * 9 / 5 + 32;
+//   fahrenheit.value = parseFloat(f.toFixed(2));
 
-  let c = (parseFloat(kelvin.value) - 273.15);
-  celsius.value = parseFloat(c.toFixed(2));
-}
+//   let c = (parseFloat(kelvin.value) - 273.15);
+//   celsius.value = parseFloat(c.toFixed(2));
+// }
